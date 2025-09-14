06d90b6c022af21e04158263f26786bfabb2b54a
Esteban <estebansant30@gmail.com>
2025-09-13 22:57:12 -0300

    finished main features section

--- components/sections/features/FeaturesCards.tsx
+++ components/sections/features/FeaturesCards.tsx
@@ -17,11 +17,11 @@ const FeaturesCards = () => {
   const tFeatures = useTranslations("Features");
   const cards = tFeatures.raw("cards") as FeatureCard[];
   return (
-    <div className="flex-center-col h-fit w-full min-w-full items-stretch gap-10 xl:flex-row">
+    <div className="flex-center-col h-full w-full min-w-full items-stretch gap-10 md:items-center md:justify-center xl:flex-row">
       {cards.map((card, index) => (
         <div
           key={index}
-          className="border-primary-text-500 bg-elevated-surfaces-500 flex w-full flex-col items-start justify-between gap-6 rounded-t-[0.8rem] border border-solid px-4 pt-4 pb-0 lg:gap-[clamp(2.4rem,1.25vw)] xl:border-b-0"
+          className="border-primary-text-500 bg-elevated-surfaces-500 flex h-full w-full flex-col items-start justify-between gap-6 rounded-t-[0.8rem] border border-solid px-4 pt-4 pb-0 md:max-w-[44rem] lg:gap-[clamp(2.4rem,1.25vw)] xl:max-w-[90rem] xl:items-stretch xl:border-b-0 2xl:gap-12"
         >
           <div className="flex-start-col gap-2">
             <p className="paragraph-24-medium text-primary-text-700 w-full text-left">
@@ -34,14 +34,14 @@ const FeaturesCards = () => {
               }}
             ></p>
           </div>
-          <div className="border-primary-text-700 relative flex h-[16rem] w-full min-w-full items-end justify-end rounded-tl-[0.4rem] rounded-tr-[0.4rem] border border-solid bg-white md:h-[24rem] lg:h-full lg:min-h-[2rem] xl:border-b-0">
-            <div className="relative h-[16rem] w-full md:h-[24rem] lg:h-full lg:min-h-[2rem]">
+          <div className="border-primary-text-700 relative flex h-[16rem] w-full min-w-full items-end justify-end rounded-tl-[0.4rem] rounded-tr-[0.4rem] border border-b-0 border-solid bg-white md:h-[24rem] xl:h-[clamp(20rem,10.4vw,60rem)]">
+            <div className="relative h-[16rem] w-full md:h-[24rem] xl:h-[clamp(20rem,10.4vw,60rem)]">
               <Image
                 src={imageData[card.image.src as keyof typeof imageData]}
                 alt={card.image.alt}
                 fill
                 sizes="(max-width: 1024px) 70vw, 25vw"
-                className="bg-bottom bg-no-repeat object-contain xl:object-cover"
+                className="bg-bottom bg-no-repeat object-cover xl:object-contain"
               />
             </div>
           </div>
